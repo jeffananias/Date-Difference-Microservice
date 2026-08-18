27e7eda2ab7748b5a6c023dc62564d9b00ee1c79
jeffananias <258986601+jeffananias@users.noreply.github.com>
2026-08-17 18:22:05 -0700

    Refactor program for portfolio

--- Date_Difference.py
+++ Date_Difference.py
@@ -1,100 +1,97 @@
-from datetime import date, datetime
+# Authors: Jeff Ananias, Mitchell Walker
+# Course: CS361
+# Assignment: 9 - Big Pool Implementation
+# Microservice: Date Difference
+
+import datetime
 import time
 
-REQUEST_FILE = 'date_diff.txt'
+REQUEST_FILE = "date_diff.txt"
 
 
-def calculate_date_difference(given_date_text: str) -> str:
+def main() -> None:
+    """Start microservice as continuous process."""
+    greet()
+    last_file_text = ""
+    while True:
+        file_text = get_file_text()
+        last_file_text = process_request(file_text, last_file_text)
+        time.sleep(0.5)
+
+
+def greet() -> None:
+    """Greet user and advise request-response format."""
+    print("\nDate Difference Microservice is running.")
+    print("Waiting for date request in date_diff.txt.")
+    print("Please use date request format: YYYY-MM-DD\n")
+
+
+def get_file_text() -> str:
     """
-    Take a date string in YYYY-MM-DD format and compare it to today's date.
-    Return a message showing if the date is in the future, past, or today.
+    Return text from REQUEST FILE if exists; else create empty file and
+    return empty string.
     """
-    # Validate format
     try:
-        given_date = datetime.strptime(given_date_text, '%Y-%m-%d').date()
-    except ValueError:
-        return 'DATE_DIFF_ERROR: Invalid date format. Use YYYY-MM-DD.'
-
-    # Positive diff is future date; negative diff is past date
-    date_difference = (given_date - date.today()).days
-
-    if date_difference > 0:
-        return 'DAYS_REMAINING: ' + str(date_difference)
-    elif date_difference < 0:
-        return 'OVERDUE: ' + str(abs(date_difference))
-    else:
-        return 'DUE_TODAY: 0'
+        with open(REQUEST_FILE, "r") as f:
+            file_text = f.read().strip()
+    except FileNotFoundError:
+        with open(REQUEST_FILE, "w") as f:
+            f.write("")
+        file_text = ""
+    return file_text
 
 
 def is_response_message(file_text: str) -> bool:
     """
-    Check response to avoid reading Date_Difference response as new request.
+    Return True if file_text is response instead of request;
+    else return False.
     """
-    if file_text.startswith('DAYS_REMAINING:'):
-        return True
-    elif file_text.startswith('OVERDUE:'):
-        return True
-    elif file_text.startswith('DUE_TODAY:'):
-        return True
-    elif file_text.startswith('DATE_DIFF_ERROR:'):
-        return True
+    res_type_1 = bool(file_text.startswith("DAYS_REMAINING:"))
+    res_type_2 = bool(file_text.startswith("OVERDUE:"))
+    res_type_3 = bool(file_text.startswith("DUE_TODAY:"))
+    res_type_4 = bool(file_text.startswith("DATE_DIFF_ERROR:"))
+    return bool(res_type_1 or res_type_2 or res_type_3 or res_type_4)
+
+
+def calculate_date_difference(given_date_text: str) -> str:
+    """
+    Return message showing if input date in YYYY-MM-DD format is
+    future, past, or today.
+    """
+    # Validate format
+    try:
+        given_date = datetime.strptime(given_date_text, "%Y-%m-%d").date()
+    except ValueError:
+        return "DATE_DIFF_ERROR: Invalid date format. Use YYYY-MM-DD."
+
+    # Positive diff is future date; negative diff is past date
+    today = datetime.date.now(tz=datetime.timezone.utc).date()
+    date_difference = (given_date - today).days
+
+    if date_difference > 0:
+        return "DAYS_REMAINING: " + str(date_difference)
+    elif date_difference < 0:
+        return "OVERDUE: " + str(abs(date_difference))
     else:
-        return False
-
-
-def greet() -> None:
-    """
-    Greet user and advise date format.
-    """
-    print('\nDate Difference Microservice is running.')
-    print('Waiting for date request in date_diff.txt.')
-    print('Please use date request format: YYYY-MM-DD\n')
+        return "DUE_TODAY: 0"
 
 
 def process_request(file_text: str, last_file_text: str) -> str:
-    """
-    Return response based on request and print and write file text if new.
-    """
-    if file_text != '' and file_text != last_file_text:
-        
+    """Return response based on request and write file text if new."""
+    if file_text != "" and file_text != last_file_text:
         # Ignore file that already has a response in it
         if is_response_message(file_text):
             return file_text
-
         # Else, treat the file contents as a new request
         else:
-            print('Request Received: ' + file_text)
-
+            print("Request Received: " + file_text)
             response = calculate_date_difference(file_text)
-
-            with open(REQUEST_FILE, 'w') as file:
+            with open(REQUEST_FILE, "w") as file:
                 file.write(response)
-
-            print('Response Sent: ' + response)
-            print()
-
+            print("Response Sent:" + response + "\n")
             return response
-
-def run_microservice() -> None:
-    """
-    Run Date_Difference microservice as continuous process.
-    """
-    greet()
-
-    last_file_text = ''
-    while True:
-        try:
-            with open(REQUEST_FILE, 'r') as file:
-                file_text = file.read().strip()
-        except FileNotFoundError:
-            with open(REQUEST_FILE, 'w') as file:
-                file.write('')
-            file_text = ''
-
-        last_file_text = process_request(file_text, last_file_text)
-
-        time.sleep(0.5)
+    return last_file_text
 
 
-if __name__ == '__main__':
-    run_microservice()
+if __name__ == "__main__":
+    main()
